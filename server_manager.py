bbf118c18b39f4c27197ee79e322bade489927a1
TimV <timothy.vida@hotmail.de>
2025-11-28 15:41:56 +0100

    Update server_manager.py

--- server_manager.py
+++ server_manager.py
@@ -124,14 +124,12 @@ class ServerManager:
             self._ensure_battleye_config(server)
 
             # Prepare DayZ start command with all parameters
-            # BEpath should include the full path to the config file
-            be_config_file = os.path.join(server.be_path, 'BEServer_x64.cfg')
             cmd = [
                 executable,
                 f'-config=serverDZ.cfg',
                 f'-port={server.server_port}',
                 f'-profiles={server.profile_path}',
-                f'-BEpath={be_config_file}',
+                f'-BEpath={server.be_path}',
                 f'-cpuCount={server.cpu_count}'
             ]
 
